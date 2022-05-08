a5126419640988fe90d60e5552d01cb832a75de5
Iliya Steblyan <stebljan@ya.ru>
2022-05-08 12:33:39 +0600

    LV08-05-22

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -36,12 +36,12 @@ class PostFormTests(TestCase):
             group=cls.group,
         )
         cls.small_gif = (
-             b'\x47\x49\x46\x38\x39\x61\x02\x00'
-             b'\x01\x00\x80\x00\x00\x00\x00\x00'
-             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-             b'\x0A\x00\x3B'
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
         )
         cls.uploaded = SimpleUploadedFile(
             name='small.gif',
@@ -79,7 +79,7 @@ class PostFormTests(TestCase):
             'text': 'Прыг-Скок',
             'group': self.TEST_ONE,
             'image': self.uploaded,
-            }
+        }
         response = self.authorized_client.post(
             reverse('posts:post_create'),
             data=form_data,
@@ -110,7 +110,7 @@ class PostFormTests(TestCase):
             'text': 'Скок-Прыг, Прыг-Скок',
             'group': self.group.id,
             'image': self.uploaded_1,
-            }
+        }
         response = self.authorized_client.post(
             reverse('posts:post_edit', kwargs={'id': self.post.id}),
             data=form_data,
@@ -140,7 +140,7 @@ class PostFormTests(TestCase):
         """Проверка комментирования поста."""
         form_data = {
             'text': 'Тестовый коммент',
-            }
+        }
         response = self.authorized_client.post(
             reverse('posts:add_comment', kwargs={'id': self.post.id}),
             data=form_data,
